25346e0af2519827bd8b95be4073e21f202ae08a
Hamzah <hamzah.ayub96@gmail.com>
2024-04-29 20:14:40 +0500

    First proper commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+const compGuess = Math.floor(Math.random() * 10 + 1);
+//console.log(`Computer's Guess: ${compGuess}`)
+const data = await inquirer.prompt([
+    {
+        name: "usersGuess",
+        type: "number",
+        message: "Guess a number (1-10) : ",
+    },
+]);
+//console.log(data.usersGuess)
+if (compGuess === data.usersGuess) {
+    console.log("You win!");
+}
+else {
+    console.log(`Computer's Guess: ${compGuess}`);
+    console.log("You've lost!");
+}
